176c065f0a7ecd32e60b4b50ab07961affb387f6
Daan Faber <daan.faber001@fclive.nl>
2022-11-16 11:34:59 +0100

    Setup router and visualization

--- router.js
+++ router.js
@@ -7,12 +7,18 @@ const util = require('./util');
 router.route('/*')
     .put((req, res) => {
         util.verifyPutRequest(req, res);
+        process.env.DATA = JSON.stringify(req.body.data);
+        
         res.sendStatus(http.status['OK']);
     })
     .get((req, res) => {
         util.verifyGetRequest(req, res);
 
-                
+        visualizer(req, res);
+    })
+    .delete((req, res) => {
+        delete process.env.DATA;
+        res.sendStatus(http.status['OK']);
     })
     .all((req, res) => {
         res.sendStatus(405); // 405 Method Not Allowed
--- visualizer.js
+++ visualizer.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-    console.log(process.env.DATA);
+const http = require('./http');
+
+module.exports = (req, res) => {
+    const dataset = JSON.parse(process.env.DATA);
+    res.status(200).render('sudoku', dataset, (err, html) => {
+        if (err)
+            return res.status(http.status['Internal Server Error']).send(err);
+        else
+            res.send(html);
+    });
 };
