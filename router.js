3c4c1624e5de3507ad36177e9e450b395a086fe2
Daan Faber <daan.faber001@fclive.nl>
2022-11-16 10:15:31 +0100

    Switched to util file for request verification

--- router.js
+++ router.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const visualizer = require('./visualizer');
+const util = require('./util');
 
 router.route('/*')
-    .post((req, res) => {
-        // handle data input
-        // call visualization
-        console.log(req.body);
-        // process.env.DATA = req.body;
-        // visualizer();
-        res.sendStatus(200);
+    .put((req, res) => {
+        util.verifyPutRequest(req, res);
+    })
+    .get((req, res) => {
+        util.verifyGetRequest(req, res);
+
+                
     })
     .all((req, res) => {
-        res.sendStatus(418);
+        res.sendStatus(405); // 405 Method Not Allowed
     });
 
 module.exports = router;
