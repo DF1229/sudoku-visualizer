13cd7d8aa2243c892cbd44af848cde65b23897da
Daan Faber <daan.faber001@fclive.nl>
2022-11-16 11:34:07 +0100

    Setup rendered views for visualization

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 
 const router = require('./router');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs')
 app.use(express.json());
 
 app.use('/', router);
